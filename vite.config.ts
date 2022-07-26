957ff51d8bcd82d72df9d024551f110cca2ebc86
lucasdRossi <rossi.lucas06@gmail.com>
2022-07-25 23:09:16 -0300

    add public folder

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       injectRegister: "auto",
       strategies: "generateSW",
       base: "/",
-      includeAssets: ["/src/assets/favicon.ico"],
+      includeAssets: ["favicon.ico"],
       manifest: {
         name: "VitePWA",
         short_name: "VitePWA",
@@ -23,12 +23,12 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "src/assets/android-chrome-192x192.png",
+            src: "android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "src/assets/android-chrome-512x512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
