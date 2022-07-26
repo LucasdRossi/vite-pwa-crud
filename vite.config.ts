4d5a034a9b15a24937aea5cd8f327f8c5af0a390
lucasdRossi <rossi.lucas06@gmail.com>
2022-07-25 23:05:13 -0300

    fix manifest icons 3

--- vite.config.ts
+++ vite.config.ts
@@ -23,22 +23,12 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "src/assets/android-chrome-192x192",
+            src: "src/assets/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "src/assets/android-chrome-512x512",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/src/assets/android-chrome-192x192",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/src/assets/android-chrome-512x512",
+            src: "src/assets/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
