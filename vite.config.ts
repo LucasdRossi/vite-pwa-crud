fc03669d6c96e2846e0f2735dddf7a46e3f4f387
lucasdRossi <rossi.lucas06@gmail.com>
2022-07-25 22:15:44 -0300

    add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+    }),
+  ],
 });
